39e927a830800c35e35ab75e7439ab50edf2c3df
John J. Woloszyk <johnjwoloszyk@me.com>
2025-06-03 08:54:02 -0400

    Update logging flags

--- protocol/osrs-228/osrs-228-internal/src/main/kotlin/net/rsprot/protocol/internal/RSProtFlags.kt
+++ protocol/osrs-228/osrs-228-internal/src/main/kotlin/net/rsprot/protocol/internal/RSProtFlags.kt
@@ -58,7 +58,7 @@ public object RSProtFlags {
     private val networkLoggingString: String =
         getString(
             "networkLogging",
-            "off",
+            "debug",
         )
 
     private val js5LoggingString: String =
